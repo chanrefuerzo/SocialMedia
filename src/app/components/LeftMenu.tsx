9c24215b39811d4aa760495b4119b10a43e16d73
chanrefuerzoofficial <chanrefuerzoofficial@gmail.com>
2024-07-10 12:07:46 +0800

    merge fix navbar and homepage spacing

---
+++ src/app/components/LeftMenu.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LeftMenu() {
+  return <div>Left Menu</div>;
+}
---
+++ src/app/components/RightMenu.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RightMenu() {
+  return <div>Right Menu</div>;
+}
