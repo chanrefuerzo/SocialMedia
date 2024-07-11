a7049f5933a9aa434517fa9537be43884cbcc053
chanrefuerzoofficial <chanrefuerzoofficial@gmail.com>
2024-07-11 16:01:01 +0800

    add shadow on card

--- src/app/components/AddPost.tsx
+++ src/app/components/AddPost.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function AddPost() {
   return (
-    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+    <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
       {/* Avatar*/}
       <Image
         src="https://images.pexels.com/photos/25396183/pexels-photo-25396183/free-photo-of-a-woman-walking-down-the-street-with-a-suitcase.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
