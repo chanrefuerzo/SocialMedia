85973005a518eda87c6bdd49290da6aa361f6781
chanrefuerzoofficial <chanrefuerzoofficial@gmail.com>
2024-07-11 20:31:49 +0800

    add comment section

--- src/app/components/Comments.tsx
+++ src/app/components/Comments.tsx
@@ -13,7 +13,7 @@ export default function Comments() {
           height={32}
           className="rounded-full w-8 h-8"
         />
-        <div className="flex flex-1 items-center justify-between bg-slate-100 rounded-xl px-6 py-2 w-full">
+        <div className="flex flex-1 items-center justify-between bg-slate-100 rounded-xl px-6 py-2 w-full ">
           <input
             name=""
             id=""
@@ -30,7 +30,52 @@ export default function Comments() {
         </div>
       </div>
       {/* Old Comment*/}
-      <div className=""></div>
+      <div className="flex justify-between gap-4 mt-6">
+        {/* Avatar Old Comment*/}
+        <Image
+          src="https://images.pexels.com/photos/20579596/pexels-photo-20579596/free-photo-of-black-and-white-blurred-photography-of-a-woman.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt="avatar-commment"
+          width={40}
+          height={40}
+          className="rounded-full w-10 h-10"
+        />
+        {/* User & Comment & likes*/}
+        <div className="flex flex-col gap-2 flex-1">
+          <span className="font-medium">Al Sagarino</span>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution
+          </p>
+          <div className="flex flex-col gap-8 text-xs text-gray-500 mt-2 ">
+            <div className="flex items-center gap-4 ">
+              <Image
+                src="/like.png"
+                alt=""
+                width={12}
+                height={12}
+                className="cursor-pointer"
+              />
+              <span className="text-gray-300">|</span>
+              <span className="text-gray-500">
+                123 <span className="hidden md:inline">Likes</span>
+              </span>
+              <div className="">Reply</div>
+            </div>
+          </div>
+        </div>
+        {/* More button*/}
+        <div className="">
+          <Image
+            src="/more.png"
+            alt=""
+            width={16}
+            height={16}
+            className="cursor-pointer w-4 h-4"
+          />
+        </div>
+      </div>
     </div>
   );
 }
