9042d2cbe40998f0a3d725be053fb8c0ce719e45
chanrefuerzoofficial <chanrefuerzoofficial@gmail.com>
2024-07-11 15:57:43 +0800

    add post ui

--- src/app/components/AddPost.tsx
+++ src/app/components/AddPost.tsx
@@ -1,5 +1,80 @@
+import Image from "next/image";
 import React from "react";
 
 export default function AddPost() {
-  return <div>Post</div>;
+  return (
+    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+      {/* Avatar*/}
+      <Image
+        src="https://images.pexels.com/photos/25396183/pexels-photo-25396183/free-photo-of-a-woman-walking-down-the-street-with-a-suitcase.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        alt="avatar-post"
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      />
+      {/* POST*/}
+      <div className="flex-1 ">
+        {/* TEXT*/}
+        <div className="flex gap-4">
+          <textarea
+            name=""
+            id=""
+            placeholder="What's on your mind?"
+            className="bg-slate-100 rounded-lg flex-1 p-2"
+          ></textarea>
+          <Image
+            src="/emoji.png"
+            alt="emoji"
+            width={20}
+            height={20}
+            className="w-5 h-5 cursor-pointer self-end"
+          />
+        </div>
+
+        {/* Options */}
+        <div className="flex gap-4 items-center mt-2 text-gray-400 flex-wrap">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image
+              src="/addImage.png"
+              alt="emoji"
+              width={20}
+              height={20}
+              className="w-5 h-5 cursor-pointer self-end"
+            />
+            Photo
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image
+              src="/addVideo.png"
+              alt="emoji"
+              width={20}
+              height={20}
+              className="w-5 h-5 cursor-pointer self-end"
+            />
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image
+              src="/poll.png"
+              alt="emoji"
+              width={20}
+              height={20}
+              className="w-5 h-5 cursor-pointer self-end"
+            />
+            Poll
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image
+              src="/addevent.png"
+              alt="emoji"
+              width={20}
+              height={20}
+              className="w-5 h-5 cursor-pointer self-end"
+            />
+            Event
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
