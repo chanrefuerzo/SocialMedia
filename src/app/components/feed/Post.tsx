7cb4b49200b944e3ab7dcdaeae2fc55de78f5464
chanrefuerzoofficial <chanrefuerzoofficial@gmail.com>
2024-09-02 17:01:16 +0800

    fix auth problem

--- src/app/components/feed/Post.tsx
+++ src/app/components/feed/Post.tsx
@@ -12,58 +12,52 @@ type FeedPostType = PostType & { user: User } & {
   _count: { comments: number };
 };
 
-const { userId } = auth();
 export default function Post({ post }: { post: FeedPostType }) {
+  const { userId } = auth();
   return (
     <div className="flex flex-col gap-4">
-      {/* User */}
+      {/* USER */}
       <div className="flex items-center justify-between">
-        {/* Avatar & Name */}
-        <div className="flex gap-4 items-center">
+        <div className="flex items-center gap-4">
           <Image
             src={post.user.avatar || "/noAvatar.png"}
-            alt=""
             width={40}
             height={40}
-            className=" rounded-full w-10 h-10"
+            alt=""
+            className="w-10 h-10 rounded-full"
           />
           <span className="font-medium">
-            {" "}
-            {post.user.name && post.user.username
+            {post.user.name && post.user.surname
               ? post.user.name + " " + post.user.surname
-              : post.user.username}{" "}
+              : post.user.username}
           </span>
         </div>
         {userId === post.user.id && <PostInfo postId={post.id} />}{" "}
       </div>
-      {/* Description */}
+      {/* DESC */}
       <div className="flex flex-col gap-4">
-        {/* Caption */}
-        <div className="">{post.desc}</div>
-        {/* Image  */}
         {post.img && (
-          <div className="w-full min-h-96 relative ">
+          <div className="w-full min-h-96 relative">
             <Image
               src={post.img}
-              alt=""
               fill
               className="object-cover rounded-md"
+              alt=""
             />
           </div>
         )}
+        <p>{post.desc}</p>
       </div>
-      {/* Interaction */}
-      <Suspense fallback="loading">
+      {/* INTERACTION */}
+      <Suspense fallback="Loading...">
         <PostInteraction
           postId={post.id}
           likes={post.likes.map((like) => like.userId)}
           commentNumber={post._count.comments}
         />
       </Suspense>
-      <Suspense fallback="loading">
-        <div className="">
-          <Comments postId={post.id} />
-        </div>
+      <Suspense fallback="Loading...">
+        <Comments postId={post.id} />
       </Suspense>
     </div>
   );
